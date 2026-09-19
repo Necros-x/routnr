6f82e16702b1b09ae1834d224d56b2266c194667
Ramika Perera <145104853+Necros-x@users.noreply.github.com>
2026-09-19 09:04:13 +0530

    data: add official MAG reference URLs

--- src/config/code.ts
+++ src/config/code.ts
@@ -1,6 +1,15 @@
 export const MAG_CODE_PDF_URL =
   'https://www.gymnastics.sport/publicdir/rules/files/en_1.1%20-%20MAG%20Code%20of%20Points%202025-2028.pdf';
 
+export const CODE_APPENDIX_PDF_URL =
+  'https://www.gymnastics.sport/publicdir/rules/files/en_1.2%20-%20Appendix%20to%20the%20Code%20of%20Points%202025-2028.pdf';
+
+export const MAG_NEWSLETTERS_URL =
+  'https://www.gymnastics.sport/site/pages/newsletters-mag.php';
+
+export const WORLD_GYMNASTICS_RULES_URL =
+  'https://www.gymnastics.sport/site/rules/index.php';
+
 export const CODE_FIGURE_BASE_PATH = '/code-figures';
 export const CODE_FIGURE_MANIFEST_PATH = `${CODE_FIGURE_BASE_PATH}/manifest.json`;
 
